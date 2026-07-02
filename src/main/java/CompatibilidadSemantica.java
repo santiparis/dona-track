aadba9b1d6fe74af5e6f5deac00967f6d085f5df
delficordero <dcordero@frba.utn.edu.ar>
2026-07-02 12:21:41 -0300

    arreglo

--- src/main/java/CompatibilidadSemantica.java
+++ src/main/java/CompatibilidadSemantica.java
@@ -4,6 +4,9 @@ public class CompatibilidadSemantica implements EstrategiaAsignacion {
 
   @Override
   public List<EntidadBeneficiaria> sugerirEntidades(DonacionIndependiente donacion, List<EntidadBeneficiaria> entidades) {
-    return entidades.stream().filter(entidad -> entidad.necesitaSubcategoria(donacion.getBien().getSubcategoria())).limit(10).toList();
+    return entidades
+            .stream()
+            .filter(entidad -> entidad.necesitaSubcategoria(donacion.getBien().getSubcategoria())).limit(10)
+            .toList();
   }
 }
--- src/main/java/PrioridadSubAtendidos.java
+++ src/main/java/PrioridadSubAtendidos.java
@@ -6,7 +6,8 @@ public class PrioridadSubAtendidos implements EstrategiaAsignacion {
 
   @Override
   public List<EntidadBeneficiaria> sugerirEntidades(DonacionIndependiente donacion, List<EntidadBeneficiaria> entidades) {
-    return entidades.stream()
+    return entidades
+            .stream()
             .sorted(Comparator.comparingLong(EntidadBeneficiaria::getDonacionesRecibidasUltimoTrimestre))
             .limit(10).collect(Collectors.toList());
   }
