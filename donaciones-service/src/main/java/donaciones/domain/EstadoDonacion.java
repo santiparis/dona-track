453a29d2e1884479259a151f88174854569c0714
JuanIgnPer <juan991969@hotmail.com>
2026-09-15 21:18:54 -0300

    Actualiza API de donaciones: modifica endpoint para cambiar estado y renombra parámetro a datosAdicionales

--- donaciones-service/src/main/java/donaciones/domain/EstadoDonacion.java
+++ donaciones-service/src/main/java/donaciones/domain/EstadoDonacion.java
@@ -2,10 +2,10 @@ package donaciones.domain;
 
 public enum EstadoDonacion {
     EN_DEPOSITO,
-    ASIGNADA,
+    ASIGNADA,                   // Nada
     LISTA_PARA_ENTREGAR,
-    EN_TRASLADO,
-    ENTREGADA,
-    ENTREGA_FALLIDA,
+    EN_TRASLADO,                // Inicio de ruta: URL de mapa interactivo
+    ENTREGADA,                  // Comprobante de entrega (Fecha y hora y camion responsable)
+    ENTREGA_FALLIDA,            // Justificacion
     VENCIDA
 }
